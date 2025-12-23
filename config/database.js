2d713de43d2d8a75a36345218d00f170dea1a6b7
riyantndjkf <153905200+riyantndjkf@users.noreply.github.com>
2025-12-23 17:24:15 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-/* config/database.js
+// config/database.js
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
@@ -19,30 +19,6 @@ const db = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-});
-
-export default db;*/
-import mysql from "mysql2/promise";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  // TAMBAHAN PENTING UNTUK MENCEGAH MALFORMED PACKET:
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
-  // Jika pakai Aiven/Cloud DB, kadang perlu SSL:
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
 });
 
 export default db;

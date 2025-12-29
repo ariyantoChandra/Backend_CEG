f9242c790595261bdb822c7be72e357949a6150f
FelixEdw <felixedwardxx@gmail.com>
2025-12-30 01:35:42 +0700

    ganti tipis config db

--- config/database.js
+++ config/database.js
@@ -5,17 +5,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 3306, // Beri default port 3306
+  
+  /* Hapus/Komentar bagian SSL di bawah ini jika menggunakan 
+     MySQL bawaan cPanel Jagoan Hosting. 
+  */
+  /*
   ssl: {
     rejectUnauthorized: false,
   },
+  */
 
-  // Opsional tapi bagus untuk performa
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,

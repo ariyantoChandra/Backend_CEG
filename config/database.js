73706fb79a230db26661253cc0d9dbbade28ede3
riyantndjkf <ariyantochandra101@gmail.com>
2026-01-01 02:20:25 +0700

    samlekum

--- config/database.js
+++ config/database.js
@@ -9,16 +9,7 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306, // Beri default port 3306
-  
-  /* Hapus/Komentar bagian SSL di bawah ini jika menggunakan 
-     MySQL bawaan cPanel Jagoan Hosting. 
-  */
-  /*
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  */
+  port: process.env.DB_PORT || 3306,
 
   waitForConnections: true,
   connectionLimit: 10,
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Routes Web Pendaftaran
 import authRoutes from "./routes/authRoutes.js";

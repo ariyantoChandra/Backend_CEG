0c1599be190ea19fd91fe19f331015c3fa02f943
sewysei <dimas.prakasa.a@gmail.com>
2026-01-04 16:00:01 +0700

    adsada

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME,

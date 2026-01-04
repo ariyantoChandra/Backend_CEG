ac4c2a7aa183a7f69d0bb906dcc614cdc7a08f6b
sewysei <dimas.prakasa.a@gmail.com>
2026-01-04 16:16:51 +0700

    Refactor database connection settings and clean up admin routes

--- config/database.js
+++ config/database.js
@@ -5,9 +5,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
 

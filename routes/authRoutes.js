b6818dd54ec8c37db853488f144cfdb12c782a99
gravict <leonardoedb17@gmail.com>
2026-01-04 15:46:03 +0700

    Delete Register Admin

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,5 @@
 import { login } from "../handler/auth/login.js";
 import { register } from "../handler/auth/register.js";
-import { registerAdmin } from "../handler/auth/registerAdmin.js";
 import { checkStatusPendaftaran } from "../handler/user/checkStatusPembayaran.js";
 import express from "express";
 // IMPORT MIDDLEWARE YANG BARU DIBUAT
@@ -13,7 +12,6 @@ router.post("/login", login);
 // TAMBAHKAN 'upload.any()' DI SINI
 // Ini berfungsi untuk menangkap semua file yang dikirim dari Front-End
 router.post("/register", upload.any(), register); 
-router.post("/register-admin", registerAdmin);
 router.get("/check-status-pendaftaran", checkStatusPendaftaran);
 
 export default router;

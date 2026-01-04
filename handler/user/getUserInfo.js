5488b4dfcdb3bcd285a68b52f8aad3a2859d7cf3
sewysei <dimas.prakasa.a@gmail.com>
2026-01-04 20:10:05 +0700

    Change get user info response

--- handler/user/getUserInfo.js
+++ handler/user/getUserInfo.js
@@ -28,7 +28,7 @@ export const getUserInfo = async (req, res) => {
     }
 
     const [info] = await db.execute(
-      "SELECT u.nama_tim, t.total_points as points, t.total_coin as coins FROM user u INNER JOIN tim t ON u.id = t.user_id WHERE u.id = ?",
+      "SELECT u.nama_tim, t.total_points as points FROM user u INNER JOIN tim t ON u.id = t.user_id WHERE u.id = ?",
       [userId]
     );
 
--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 // Endpoint cek server
 app.get("/", (req, res) => {
   res.status(200).json({
-    message: "CAMBUKKKKKKK YANTOOOO",
+    message: "CAMBUKKKKKKK YANTOOOO AHHHHHHH ",
   });
 });
 

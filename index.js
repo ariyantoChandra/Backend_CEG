754c967a7d86ad638a60b6f89a912aad63d6b30a
sewysei <dimas.prakasa.a@gmail.com>
2026-01-04 18:23:19 +0700

    asdasdasd

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 // Endpoint cek server
 app.get("/", (req, res) => {
   res.status(200).json({
-    message: "Amri siap digunakan! encrut dalam",
+    message: "Amri siap digunakan! encrut ceg2026",
   });
 });
 

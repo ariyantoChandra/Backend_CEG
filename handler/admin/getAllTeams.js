a0877731395b9a7b2a694b70f695f7928a61374e
gravict <leonardoedb17@gmail.com>
2026-01-04 14:48:53 +0700

    Mendapatkan Get All Teams

--- handler/admin/getAllTeams.js
+++ handler/admin/getAllTeams.js
@@ -5,7 +5,6 @@ export const getAllTeams = async (req, res) => {
     // Mengambil data tim dan hitung jumlah anggota per tim
     const [teams] = await db.execute(`
       select 
-        t.id,
         t.user_id,
         u.nama_tim,
         t.asal_sekolah,
@@ -14,8 +13,8 @@ export const getAllTeams = async (req, res) => {
       from tim t
       join user u on t.user_id = u.id
       left join member m on t.user_id = m.tim_user_id
-      group by t.id
-      order by t.id desc
+      group by t.user_id
+      order by t.user_id desc
     `);
 
     return res.status(200).json({

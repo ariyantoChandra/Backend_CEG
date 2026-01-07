f1f703588443406279f22789a05ae6b00846a373
riyantndjkf <riyantkun@gmail.com>
2026-01-07 13:12:22 +0700

    Update getScore.js

--- handler/penpos/getScore.js
+++ handler/penpos/getScore.js
@@ -1,4 +1,4 @@
-import db from "../../config/database";
+import db from "../../config/database.js";
 import { checkToken } from "../../config/checkToken.js";
 
 export const getScore = async (req, res) => {

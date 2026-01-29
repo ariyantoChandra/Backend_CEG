d9c67ca3bb525a633d3c562547b565ead52901b0
sewysei <dimas.prakasa.a@gmail.com>
2026-01-29 09:53:47 +0700

    Add getLeaderboard endpoint and update root response message

---
+++ handler/user/getLeaderboard.js
@@ -0,0 +1,60 @@
+import db from "../../config/database.js";
+import { checkToken } from "../../config/checkToken.js";
+
+export const getLeaderboard = async (req, res) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
+        success: false,
+        message: "Token tidak ditemukan!",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+    const { valid, expired, decoded } = checkToken(token);
+
+    if (!valid) {
+      return res.status(401).json({
+        success: false,
+        message: expired ? "Token kadaluarsa." : "Token tidak valid.",
+      });
+    }
+
+    const userId = decoded.id;
+    if (!userId) {
+      return res.status(401).json({
+        success: false,
+        message: "ID pengguna tidak valid.",
+      });
+    }
+
+    // UPDATE QUERY: Mengambil field status_pembayaran, notes, paket
+    const [info] = await db.execute(
+      `SELECT 
+        u.nama_tim, 
+        t.total_points as points,
+       FROM user u 
+       INNER JOIN tim t ON u.id = t.user_id`,
+    );
+
+    if (info.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User tidak ditemukan!",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Berhasil mendapatkan leaderboard!",
+      data: info[0],
+    });
+  } catch (error) {
+    console.error("ERROR GET USER LEADERBOARD:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Terjadi kesalahan server: " + error.message,
+    });
+  }
+};
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,13 +2,14 @@ import express from "express";
 import { verifyPayment } from "../handler/admin/verifyPayment.js";
 import { getAllTeams } from "../handler/admin/getAllTeams.js";
 import { getTeamDetail } from "../handler/admin/getTeamDetail.js";
+import { getLeaderboard } from "../handler/user/getLeaderboard.js";
 import { upload } from "../middleware/uploadImage.js";
 
 const router = express.Router();
 
 router.get("/get-all-teams", getAllTeams);
-// ====================================================
-
 router.get("/get-team-detail/:teamId", getTeamDetail);
 router.put("/verify-payment/:teamId", upload.any(), verifyPayment);
+router.get("/leaderboard", getLeaderboard);
+
 export default router;
